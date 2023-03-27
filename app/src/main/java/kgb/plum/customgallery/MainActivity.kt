2025299ac1f3c7848ffad834e56d6ab4bfb60c7f
agfalcon <agfalcon12@gmail.com>
2023-03-27 17:46:44 +0900

    🎉 Custom Galley Project Init!!

---
+++ app/src/main/java/kgb/plum/customgallery/MainActivity.kt
@@ -0,0 +1,11 @@
+package kgb.plum.customgallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
