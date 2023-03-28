414fca16526fe5def31b0b3e66519c2dc2e785a1
agfalcon <agfalcon12@gmail.com>
2023-03-28 09:57:47 +0900

    🎉 갤러리 권한 가져오기

--- app/src/main/java/kgb/plum/customgallery/MainActivity.kt
+++ app/src/main/java/kgb/plum/customgallery/MainActivity.kt
@@ -1,11 +1,79 @@
 package kgb.plum.customgallery
 
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kgb.plum.customgallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val imageLoadLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()){
+        uriList ->
+        updateImages(uriList)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loadImageButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission(){
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                showPermissionInfoDialog()
+            }
+            else -> {
+                requestReadExternalStoragePermission()
+            }
+        }
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply{
+            setMessage("이미지를 가져오기 위해서, 외부 저장소 읽기 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의"){ _, _ ->
+                requestReadExternalStoragePermission()
+            }.show()
+        }
+    }
+
+    private fun requestReadExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private fun updateImages(uriList: List<Uri>){
+        Log.i("테스트", uriList.toString())
+    }
+
+
+    private fun loadImage() {
+        imageLoadLauncher.launch("image/*")
+    }
+
+    companion object{
+        const val REQUEST_READ_EXTERNAL_STORAGE = 99
     }
 }
